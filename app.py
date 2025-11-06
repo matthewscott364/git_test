dc392d43b7e985b1a50d5e3d795018bfd2435df3
Matthew Scott <matthewscott364@gmail.com>
2025-11-06 02:33:58 -0500

    new files updated

--- app.py
+++ app.py
@@ -10,3 +10,10 @@ def hello_world():
     return 'Hello, World!'
 if __name__ == '__main__':
     app.run()
+
+## This is a change to our code 
+@app.route('/goodbye')
+def goodbye_world():
+    return 'Goodbye, World!'
+if __name__ == '__main__':
+    app.run()
