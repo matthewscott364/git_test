850509e321991a939a6449983f3318d990626a56
Matthew Scott <matthewscott364@gmail.com>
2025-11-06 01:51:07 -0500

    second commit with file

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
 ## This is just a test title to check the completion functionality
+
+## Really dumb code snippet
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+if __name__ == '__main__':
+    app.run()
