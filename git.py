d3e9ce826f2a658b8e2e8665dcb3fe1776893251
Matthew Scott <matthewscott364@gmail.com>
2025-11-06 03:00:12 -0500

    added code

--- git.py
+++ git.py
@@ -1,1 +1,10 @@
-## Test
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+def example_function():
+    print("This is an example function.")
+    data = pd.DataFrame({
+        'A': np.random.rand(10),
+        'B': np.random.rand(10)
+    })
